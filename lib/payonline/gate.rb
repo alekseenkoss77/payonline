3f1fe44ecfe3cd42c9d8ccdeab5d2b96495fac1c
Alekseenko Sergey <alekseenkoss@gmail.com>
2014-07-14 15:07:09 +0400

    1

--- lib/payonline/gate.rb
+++ lib/payonline/gate.rb
@@ -1,9 +1,14 @@
 module Payonline
   class Gate
+    attr_reader :order_id, :amount_id
 
-    def initialize(order_id, amount_id)
+    def initialize(order_id, amount_id, options={})
       @order_id = order_id
       @amount_id = amount_id
+    end
+
+    def link
+      link = Payonline.configuration.url
     end
 
     class << self
