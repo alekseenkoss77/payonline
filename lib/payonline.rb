3173ab8d0e2c07c5d8bc1e6facbe43e59b80cc12
Alekseenko Sergey <alekseenkoss@gmail.com>
2014-07-14 15:41:03 +0400

    1

--- lib/payonline.rb
+++ lib/payonline.rb
@@ -2,13 +2,14 @@ require 'payonline/gate'
 
 module Payonline
   class Configuration
-    attr_accessor :merchant_id, :currency, :url, :lang
+    attr_accessor :merchant_id, :currency, :url, :lang, :private_security_key
 
     def initialize
       @merchant_id = 12345
       @currency = 'RUB' # 'RUB', 'EUR', 'USD'
       @lang = 'ru'
       @url = "https://secure.payonlinesystem.com/#{lang}/payment/"
+      @private_security_key = 'fuuu123'
     end
   end
 
--- lib/payonline/gate.rb
+++ lib/payonline/gate.rb
@@ -1,14 +1,23 @@
 module Payonline
   class Gate
-    attr_reader :order_id, :amount_id
+    include ActiveModel::Validations
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
 
-    def initialize(order_id, amount_id, options={})
-      @order_id = order_id
-      @amount_id = amount_id
+    attr_accessor :order_id, :amount, :lang, :order_description,
+                  :valid_until, :return_url, :fail_url, :merchant_id
+
+    def initialize(attributes={})
+      attributes.each do |key,value|
+        send("#{key}",value)
+      end
+      merchant_id = Payonline.configuration.merchant_id
+      Payonline.configuration.lang ||= lang
     end
 
     def link
       link = Payonline.configuration.url
+      
     end
 
     class << self
