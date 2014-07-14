44198ea9f5e56b49d7913124268fb47f081e8a86
Alekseenko Sergey <alekseenkoss@gmail.com>
2014-07-14 17:20:50 +0400

    2'

--- lib/payonline/gate.rb
+++ lib/payonline/gate.rb
@@ -2,6 +2,7 @@ module Payonline
   class Gate
     include ActiveModel::Model
 
+
     attr_accessor :order_id, :amount, :lang, :order_description,
                   :valid_until, :return_url, :fail_url, :merchant_id,
                   :private_security_key
@@ -18,14 +19,18 @@ module Payonline
 
     def link
       link = Payonline.configuration.url
-      [:merchant_id, :order_id, :amount, :currency, :valid_until, :order_description, :private_security_key].each_with_index do |method, index|
+      [:merchant_id, :order_id, :amount, :currency, :valid_until, :order_description].each_with_index do |method, index|
         if index == 0
           link += "?MerchantId=#{send(method)}" 
         else
           link += "&#{method.to_s.classify}=#{send(method)}" if self.class.attribute_method? method
         end
       end
-      link
+      # get md5 hash of link url
+      link_private = link + "&PrivateSecurityKey=#{private_security_key}"
+      md5 = Digest::MD5
+      security_key = md5.hexdigest link_private
+      link += "&SecurityKey=#{security_key}"
     end
 
     class << self
