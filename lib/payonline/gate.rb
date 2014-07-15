80600a0517c8e59218c9947042c748312dd8b26a
Alekseenko Sergey <alekseenkoss@gmail.com>
2014-07-15 15:03:32 +0400

    2

--- lib/payonline/gate.rb
+++ lib/payonline/gate.rb
@@ -1,11 +1,46 @@
 module Payonline
   class Gate
+    # TODO: make strong validations
     include ActiveModel::Model
 
 
     attr_accessor :order_id, :amount, :lang, :order_description,
                   :valid_until, :return_url, :fail_url, :merchant_id,
-                  :private_security_key, :currency
+                  :private_security_key, :currency, :transaction_id
+
+    #####
+    #
+    # Payonline::Gate - special payment gateway for payonline.ru service
+    # for using this, in your controller you should create new payment object
+    # Example for usage:
+    # 
+    # => in contollers/examples_controller.rb
+    # def payment
+    #   @order = Order.find params[:id]
+    #
+    #   @payment = Payonline::Gate.new(
+    #     :order_id => @order.id,
+    #     :amount => @order.amount,
+    #     :currency => "EUR",
+    #     :order_description => "Thid is t-short" 
+    #   )
+    # end
+    #
+    # => in views/examples/payment.html.erb
+    #  <div class="payment">
+    #     <a href="<% @payment.link %>"> Payment link </a>
+    #  </div>
+    #
+    # => @payment.currency  may be "RUB", "EUR", "USD"
+    # => @payment.amount    decimal value (e.g. 15000)
+    # => @payment.order_description   description of order shuld be 100 chars max length
+    #
+    # TODO: need more documentation =)
+    #
+    #
+    #
+    ######
+
 
     def initialize(attributes={})
       attributes.each do |key,value|
@@ -28,11 +63,29 @@ module Payonline
           link += "&#{method.to_s.classify}=#{val}" if val.present? 
         end
       end
-      # get md5 hash of link url
+
       link_private = link + "&PrivateSecurityKey=#{private_security_key}"
+      link = Payonline.configuration.url + "?" + link + "&SecurityKey=#{self.security_key(link_private)}"
+    end
+
+    def security_key(link)
       md5 = Digest::MD5
-      security_key = md5.hexdigest link_private
-      link = Payonline.configuration.url + "?" + link + "&SecurityKey=#{security_key}"
+      security_key = md5.hexdigest link
+    end
+
+    def check_status
+      content_type = '&ContentType=xml'
+      link = ""
+      [:merchant_id, :order_id, :transaction_id].each_with_index do |method, index|
+        if index == 0
+          link += "MerchantId=#{send(method)}" 
+        else
+          val = send(method) if self.class.attribute_method? method
+          link += "&#{method.to_s.classify}=#{val}" if val.present? 
+        end
+      end
+      link_private = link + "&PrivateSecurityKey=#{private_security_key}"
+      result_link = "https://secure.payonlinesystem.com/payment/search?" + link + "&SecurityKey=#{self.security_key}" + content_type
     end
 
     class << self
